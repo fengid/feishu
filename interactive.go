e5343924658a988d4d5581a0b9ec02883e3a8955
fengid <yaogan@qingmutec.com>
2021-12-31 17:40:07 +0800

    新增 “获取指定消息的内容”接口

--- interactive.go
+++ interactive.go
@@ -44,3 +44,68 @@ func (c *Client) InteractiveV1CardUpdate(param InteractiveV1CardUpdateParam) (*I
 	}
 	return &data, err
 }
+
+type MessageParam struct {
+	MessageId string
+}
+
+type MessageRes struct {
+	ResponseCode
+	Data MessageResData `json:"data"`
+}
+
+type MessageResData struct {
+	Items []MessageResDataItem `json:"items"`
+}
+
+type MessageResDataItem struct {
+	MessageId      string                       `json:"message_id"`
+	RootId         string                       `json:"root_id"`
+	ParentId       string                       `json:"parent_id"`
+	MsgType        string                       `json:"msg_type"`
+	CreateTime     string                       `json:"create_time"`
+	UpdateTime     string                       `json:"update_time"`
+	Deleted        bool                         `json:"deleted"`
+	Updated        bool                         `json:"updated"`
+	ChatId         string                       `json:"chat_id"`
+	Sender         MessageResDataItemSender     `json:"sender"`
+	Body           MessageResDataItemBody       `json:"body"`
+	Mentions       []MessageResDataItemMentions `json:"mentions"`
+	UpperMessageId string                       `json:"upper_message_id"`
+}
+
+type MessageResDataItemSender struct {
+	Id         string `json:"id"`
+	IdType     string `json:"id_type"`
+	SenderType string `json:"sender_type"`
+}
+
+type MessageResDataItemBody struct {
+	Content string `json:"content"`
+}
+
+type MessageResDataItemMentions struct {
+	Key    string `json:"key"`
+	Id     string `json:"id"`
+	IdType string `json:"id_type"`
+	Name   string `json:"name"`
+}
+
+// Messages 获取指定消息的内容
+func (c *Client) Messages(param MessageParam) (*MessageRes, error) {
+	request, _ := http.NewRequest(http.MethodGet, ServerUrl+"/open-apis/im/v1/messages/"+param.MessageId, nil)
+	AccessToken, err := c.TokenManager.GetAccessToken()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.Do(request, AccessToken)
+	if err != nil {
+		return nil, err
+	}
+	var data MessageRes
+	err = json.Unmarshal(resp, &data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, err
+}
